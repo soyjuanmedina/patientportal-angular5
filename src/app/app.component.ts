bb2d0d39c6668b9916abd97bb7aba27f965e1acf
Juan Medina <soyjuanmedina@gmail.com>
2018-04-26 13:25:26 +0200

    Dinamic title and favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {TranslateService} from 'ng2-translate';
+import { Title } from '@angular/platform-browser';
+import { AppSettings } from '../app/appSettings';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +11,30 @@ import {TranslateService} from 'ng2-translate';
 export class AppComponent {
   title = 'Patient Portal';
 
-  constructor(public translate: TranslateService) {
+  constructor(public translate: TranslateService,
+   private titleService: Title) {
+
+    this.titleService.setTitle(AppSettings.SITETITLE);
+
+    this.changeFavicon(AppSettings.FAVICON);
+
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('en');
 
      // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('en');
   }
+
+  changeFavicon(src) {
+    var link = document.createElement('link'),
+      oldLink = document.getElementById('dynamic-favicon');
+    link.id = 'dynamic-favicon';
+    link.rel = 'shortcut icon';
+    link.href = src;
+    if (oldLink) {
+      document.head.removeChild(oldLink);
+    }
+    document.head.appendChild(link);
+  }
+
 }
--- src/app/appSettings.ts
+++ src/app/appSettings.ts
@@ -5,12 +5,16 @@ export class AppSettings {
   public static ADDRESS = 'Adress - City - Country';
   public static NAME = 'Clinic Name';
   public static PHONE = '+34 666 666 666';
-  public static EMAIL = 'email@email.com';
+  public static EMAIL = 'email@email.com'  
+  public static SITETITLE = 'Patient Portal - CommonsMS';
+  public static FAVICON = 'assets/img/favicon.gif';;
 
   // public static LOGO = 'assets/img/logo-schon-clinic-trans.png';
   // public static ADDRESS = '11 Weymouth Street - London - UK';
   // public static NAME = 'Schoen Clinic';
   // public static PHONE = '+44 20 3355 2693';
   // public static EMAIL = 'info@schoen-clinic.co.uk';
+  // public static SITETITLE = 'Patient Portal - Schoen Clinic';
+  // public static FAVICON = 'assets/img/faviconSK.png';
 
 }
